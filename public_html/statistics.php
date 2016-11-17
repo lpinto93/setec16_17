179d05b62c6dbbf3c077fbc23b66502c424405b0
lpinto93 <ee11169@fe.up.pt>
2016-11-17 14:42:13 +0000

    New statistic graph

--- public_html/statistics.php
+++ public_html/statistics.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -10,7 +11,7 @@
 <link href="default.css" rel="stylesheet" type="text/css" media="all" />
 <link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
 </head>
-    
+
 <body>
 <div id="header-wrapper">
 	<div id="header" class="container">
@@ -28,5 +29,60 @@
 	</div>
 </div>
 
+<div id="chartContainer" style="height: 400px; width: 100%;">
+        <script type="text/javascript">
+  window.onload = function () {
+
+    var chart = new CanvasJS.Chart("chartContainer",
+    {
+      zoomEnabled: true,
+      title:{
+        text: "Try Zooming And Panning (with dates)"
+      },
+      data: [						//data,  // random generator below
+	  {
+		type: "line",
+		
+		dataPoints: [
+        { x: new Date(2012, 00, 1), y: 450 },
+        { x: new Date(2012, 01, 1), y: 414 },
+        { x: new Date(2012, 02, 1), y: 520 },
+        { x: new Date(2012, 03, 1), y: 460 },
+        { x: new Date(2012, 04, 1), y: 450 },
+        { x: new Date(2012, 05, 1), y: 500 },
+        { x: new Date(2012, 06, 1), y: 480 },
+        { x: new Date(2012, 07, 1), y: 480 },
+        { x: new Date(2012, 08, 1), y: 410 },
+        { x: new Date(2012, 09, 1), y: 500 },
+        { x: new Date(2012, 10, 1), y: 480 },
+        { x: new Date(2012, 11, 1), y: 510 }
+        ]
+		
+	  }
+	  ]
+	  });
+
+    chart.render();
+  }
+
+   /*var limit = 1000;    //increase number of dataPoints by increasing this
+
+    var y = 0;
+    var data = []; var dataSeries = { type: "line" };
+    var dataPoints = [];
+    for (var i = 0; i < limit; i += 1) {
+        y += (Math.random() * 10 - 5);
+         dataPoints.push({
+          x: i - limit / 2,
+          y: y
+           });
+        }
+     dataSeries.dataPoints = dataPoints;
+     data.push(dataSeries);*/
+
+	</script>
+	<script src="canvasjs.min.js"></script>
+	<title>CanvasJS Example</title>
+	</div>
 </body>
 </html>
